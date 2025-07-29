0fef35cad1abbb0d6aab843f2cc0fa12c5e09287
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-07-29 16:43:38 +0700

    feat: update text theme to 'prompt' with GoogleFonts and update theme color to blue

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker_app/pages/expense_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; // Import google_fonts
 
 void main() {
   runApp(const ExpenseTracker());
@@ -12,10 +13,15 @@ class ExpenseTracker extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue[700]!,
+        ),
+        // Apply Google Fonts to the entire text theme
+        textTheme: GoogleFonts.promptTextTheme(),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: ExpensePage(),
+      home: const ExpensePage(),
     );
   }
 }
